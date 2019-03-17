57fba21f4a338dd27dd904f2805d6b686b66045f
Leon Zhang <leon.answer@gmail.com>
2019-03-17 17:49:40 -0400

    add config

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
-console.log('process.env.PORT', process.env);
+// console.log('process.env.PORT', process.env);
 const PORT = process.env.PORT || 5000;
 app.listen(5000);
